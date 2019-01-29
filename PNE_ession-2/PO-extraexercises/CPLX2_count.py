7de10bfbf3261f9651604c22d684622f80d8b925
aromanroman <amandaromanroman@gmail.com>
2019-01-29 16:20:29 +0100

    The optional exercises done in the session 3.

---
+++ PNE_ession-2/PO-extraexercises/CPLX2_count.py
@@ -0,0 +1,25 @@
+with open("CPLX2.txt","r") as f:
+    counter_a = 0
+    counter_c =0
+    counter_t = 0
+    counter_g = 0
+    for line in f:
+        if line[0]==">":
+
+            continue
+
+        counter_a += line.count("A")
+        counter_c += line.count("C")
+        counter_t += line.count("T")
+        counter_g += line.count("G")
+
+
+    print("the counter of A is: ",counter_a)
+    print("the counter of T is: ", counter_t)
+    print("the counter of G is: ", counter_g)
+    print("the counter of C is: ", counter_c)
+
+
+
+
+
