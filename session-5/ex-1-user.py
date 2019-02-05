d22e3f3febace5c4256fbd6302462a357d742e8c
aromanroman <amandaromanroman@gmail.com>
2019-02-05 16:42:57 +0100

    improving the ex-1.py and changing its name

--- session-5/ex-1-user.py
+++ session-5/ex-1-user.py
@@ -9,9 +9,10 @@ def count_a(seq):
     return result
 
 
+
 # Main program
 
-s = "AGTACACTGGT"
+s = input("Introduce a sequence")
 na = count_a(s)
 print("The are {} As in the sequence".format(na))
 
