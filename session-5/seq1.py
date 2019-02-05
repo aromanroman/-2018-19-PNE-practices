3c5c763fcdc67667fa672c77ddb9fe1df68b73f5
aromanroman <amandaromanroman@gmail.com>
2019-02-05 16:52:40 +0100

    the beta version of the exercise

--- session-5/seq1.py
+++ session-5/seq1.py
@@ -1,8 +1,26 @@
 class Seq:
     """A class for representing sequences"""
+    def __init__(self, strbases):
+        print("New empty sequence created!")
+        self.strbases=strbases
+    def len(self):
+        return len(self.strbases)
+
+class Gene(Seq):
+    #this class is derived from the Seq. All the objects of class Gene will inheritage the methods from Seq Class
     pass
 
+s1= Gene("ATTCGATCC")
+s2= Seq("AAAGG")
 
-s1= Seq()
-s2= Seq()
-print("the end")
+str1= s1.strbases
+str2= s2.strbases
+
+l1= s1.len()
+l2= s2.len()
+
+print("Sequence 1: {}".format(str1))
+print("lenght 1: {}".format(l1))
+print("Sequence 2: {}".format(str2))
+print("lenght 2: {}".format(l2))
+
