4b6c5a407e537f0aec7d4a6397d1165a4692eb0a
aromanroman <46930562+aromanroman@users.noreply.github.com>
2019-03-15 12:16:49 +0100

    Add files via upload

---
+++ s15/ex2.py
@@ -0,0 +1,73 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8009
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        if self.path == "/":
+            f = open("form2.html", "r")
+            contents = f.read()
+        elif "/echo" in self.path:
+            message = self.path.split("=")[1]
+            if "chk=on" in self.path:
+                message1 = self.path.split("=")[1]
+                message2 = message1.split("&")[0]
+                message3 = message2.upper()
+                contents = """ <html><body>""" + "The message introduced in capital letters is: " + message3 + """ <br><a href="/"[Main page]</a></body></html> """
+            else:
+                message1 = self.path.split("=")[1]
+                message2 = message1.split("&")[0]
+                contents = """ <html><body>""" + "The message introduced is: " + message2 + """ <br><a href="/"[Main page]</a></body></html> """
+        else:
+            f = open("error.html", "r")
+            contents = f.read()
+
+ # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
